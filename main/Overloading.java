26ebeeb2a6202799b103befd52119cba4dfe0042
Manas Upadhyay <you@example.com>
2026-09-09 15:16:34 +0530

    Clean main folder

---
+++ main/Overloading.java
@@ -0,0 +1,31 @@
+public class Overloading {
+
+    public static void main(String[] args) {
+
+        Calculator obj = new Calculator();
+
+        System.out.println("Addition of 2 numbers = " + obj.add(10, 20));
+
+        System.out.println("Addition of 3 numbers = " + obj.add(10, 20, 30));
+
+        System.out.println("Addition of 2 double numbers = " + obj.add(10.5, 20.5));
+    }
+}
+
+class Calculator {
+
+    // Method with 2 int parameters
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method with 3 int parameters
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method with 2 double parameters
+    double add(double a, double b) {
+        return a + b;
+    }
+}
